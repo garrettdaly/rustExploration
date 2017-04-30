602efe56561b4d03f46857ce33e3d46abd9727c5
John Cornish <cornish@mymail.mines.edu>
2017-04-29 18:38:53 -0600

    First run

---
+++ rust_exploration/target/debug/rust_exploration.d
@@ -0,0 +1,1 @@
+/home/hawkens/classes/CSCI400Partner/rustExploration/rust_exploration/target/debug/rust_exploration: /home/hawkens/classes/CSCI400Partner/rustExploration/rust_exploration/src/main.rs
